02137621f68338a497340f6bcb3542af1425a52d
dumisa-sakhile <contact.dumisa@gmail.com>
2025-05-29 00:45:15 +0200

    test mobile device card

--- src/components/CardLink.tsx
+++ src/components/CardLink.tsx
@@ -20,7 +20,7 @@ const CardLink: React.FC<CardLinkProps> = ({
     <Link
       to={to}
       params={params}
-      className="w-[300px] h-[450px] sm:w-[300px] sm:h-[450px] max-sm:w-[220px] max-sm:h-[360px]
+      className="w-[280px] h-[420px] sm:w-[280px] sm:h-[420px] max-sm:w-[180px] max-sm:h-[300px]
         flex-none rounded-lg shadow-md flex items-center justify-center relative group
         hover:scale-95 transition-transform duration-300 ease-in-out overflow-hidden
         geist-light hover:ring-1 hover:ring-black hover:rotate-3">
@@ -29,11 +29,11 @@ const CardLink: React.FC<CardLinkProps> = ({
         alt={title}
         className="w-full h-full object-cover rounded-lg overflow-hidden"
       />
-      <div className="absolute inset-0 bg-gradient-to-t from-black transition-opacity flex flex-col justify-end p-4 rounded-lg">
-        <h3 className="text-white text-lg sm:text-lg max-sm:text-base roboto-condensed-bold">
+      <div className="absolute inset-0 bg-gradient-to-t from-black transition-opacity flex flex-col justify-end p-3 rounded-lg max-sm:p-2">
+        <h3 className="text-white text-base sm:text-base max-sm:text-sm roboto-condensed-bold">
           {title}
         </h3>
-        <p className="text-white text-sm max-sm:text-xs roboto-condensed-light">
+        <p className="text-white text-xs max-sm:text-[10px] roboto-condensed-light">
           {description}
         </p>
       </div>
