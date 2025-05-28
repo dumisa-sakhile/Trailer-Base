7525df805265b55efa96d7c777142a4d2ee6283a
dumisa-sakhile <contact.dumisa@gmail.com>
2025-05-29 00:48:09 +0200

    2nd card test on mobile devices

--- src/components/CardLink.tsx
+++ src/components/CardLink.tsx
@@ -20,20 +20,20 @@ const CardLink: React.FC<CardLinkProps> = ({
     <Link
       to={to}
       params={params}
-      className="w-[280px] h-[420px] sm:w-[280px] sm:h-[420px] max-sm:w-[180px] max-sm:h-[300px]
-        flex-none rounded-lg shadow-md flex items-center justify-center relative group
+      className="w-[260px] h-[390px] sm:w-[260px] sm:h-[390px] max-sm:w-[150px] max-sm:h-[240px]
+        flex-none rounded-md shadow-sm flex items-center justify-center relative group
         hover:scale-95 transition-transform duration-300 ease-in-out overflow-hidden
-        geist-light hover:ring-1 hover:ring-black hover:rotate-3">
+        geist-light hover:ring-1 hover:ring-black hover:rotate-2">
       <img
         src={imageUrl}
         alt={title}
-        className="w-full h-full object-cover rounded-lg overflow-hidden"
+        className="w-full h-full object-cover rounded-md overflow-hidden"
       />
-      <div className="absolute inset-0 bg-gradient-to-t from-black transition-opacity flex flex-col justify-end p-3 rounded-lg max-sm:p-2">
-        <h3 className="text-white text-base sm:text-base max-sm:text-sm roboto-condensed-bold">
+      <div className="absolute inset-0 bg-gradient-to-t from-black transition-opacity flex flex-col justify-end p-2 rounded-md max-sm:p-1.5">
+        <h3 className="text-white text-sm sm:text-sm max-sm:text-xs roboto-condensed-bold">
           {title}
         </h3>
-        <p className="text-white text-xs max-sm:text-[10px] roboto-condensed-light">
+        <p className="text-white text-[10px] max-sm:text-[8px] roboto-condensed-light">
           {description}
         </p>
       </div>
