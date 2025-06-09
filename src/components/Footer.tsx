a023d42e6f5bcc2d59f44565bd7ea1c2df2656d2
dumisa-sakhile <contact.dumisa@gmail.com>
2025-06-10 00:28:54 +0200

    improve mobile footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-[#333]/50 backdrop-blur-md text-base text-gray-100 rounded-lg shadow-sm m-4 roboto-condensed-light md:geist-regular text-center mx-auto max-w-[1100px]">
+    <footer className="bg-[#333]/50 backdrop-blur-md text-base text-gray-100 rounded-lg shadow-sm m-4 roboto-condensed-light md:geist-regular text-center mx-auto max-w-full md:max-w-[1100px]">
       <div className="w-full p-4 grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
         {/* Left Section */}
-        <div className="text-sm text-gray-200 whitespace-nowrap">
+        <div className="text-sm text-gray-200 whitespace-nowrap md:text-left">
           © {new Date().getFullYear()}{" "}
           <a href="#" className="hover:text-blue-500">
             Trailer Base
@@ -13,7 +13,7 @@ const Footer: React.FC = () => {
           . All Rights Reserved.
         </div>
         {/* Center Section */}
-        <div className="text-sm text-gray-200 whitespace-nowrap">
+        <div className="text-sm text-gray-200 whitespace-nowrap md:text-center">
           Made with ❤️ by{" "}
           <a
             href="https://sakhile-dumisa.vercel.app/"
@@ -24,8 +24,8 @@ const Footer: React.FC = () => {
           </a>
         </div>
         {/* Right Section */}
-        <div className="flex justify-end">
-          <ul className="flex flex-wrap items-center text-sm font-medium text-gray-200 space-x-4 md:space-x-6">
+        <div className="flex justify-center md:justify-end">
+          <ul className="flex flex-wrap items-center justify-center text-sm font-medium text-gray-200 space-x-4 md:space-x-6">
             <li>
               <a
                 href="https://github.com/dumisa-sakhile"
