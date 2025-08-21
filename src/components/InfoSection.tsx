b965d65a6f2d15444e1e8e0ca415c393d24219d4
dumisa-sakhile <contact.dumisa@gmail.com>
2025-08-21 15:57:17 +0200

    update InfoSection

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -56,7 +56,7 @@ export default function InfoSection({
   if (!items.length) return null;
 
   return (
-    <section className="flex items-center gap-2 flex-wrap px-4 max-sm:px-2">
+    <section className="flex items-center  gap-2 flex-wrap px-4 max-sm:px-2 w-full justify-center md:justify-start md:items-start">
       <button className="button-style" aria-label={title}>
         {icons[title]}
         <span className="text-md  capitalize">{title}</span>
